7098e8ef469f40544ca3704401d4172aa1c0d8ed
Valentin Misiaszek <mowerv6@gmail.com>
2020-03-24 00:45:40 +0100

    feat(env): handle error if environement is undefined

--- startup/env.js
+++ startup/env.js
@@ -2,7 +2,10 @@ const config = require("config");
 
 module.exports = function() {
   if (process.env.JWT_PRIVATE_KEY === undefined)
-    throw new Error("FATAL ERROR: jwtPrivateKey is not defined in env");
+    throw new Error("FATAL ERROR: jwtPrivateKey is undefined");
+
+  if (!process.env.ENVIRONMENT)
+    throw new Error("FATAl ERROR: Environment undefined");
 
   if (process.env.ENVIRONMENT === "prod" && config.authRequired === false)
     throw new Error("FATAL ERROR: Auth is unrequired in prod environement");
