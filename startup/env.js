677813a7d9a0a11f7aab247acfddcdcf42f0597f
Valentin Misiaszek <mowerv6@gmail.com>
2020-03-24 00:42:08 +0100

    feat(env): handle error if auth is unrequired in prod env

--- startup/env.js
+++ startup/env.js
@@ -1,5 +1,9 @@
+const config = require("config");
+
 module.exports = function() {
-  if (process.env.JWT_PRIVATE_KEY === undefined) {
+  if (process.env.JWT_PRIVATE_KEY === undefined)
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined in env");
-  }
+
+  if (process.env.ENVIRONMENT === "prod" && config.authRequired === false)
+    throw new Error("FATAL ERROR: Auth is unrequired in prod environement");
 };
